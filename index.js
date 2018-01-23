ab36a42b0b346b0955d48509240c8f1fda27f0a0
Mike Waterworth <mike.waterworth@epimorphics.com>
2018-01-23 14:56:46 +0000

    Add Heroku config

--- index.js
+++ index.js
@@ -119,4 +119,6 @@ app.get('/image/', async (req, res) => {
   console.timeEnd('leaflet-image:' + req.id);
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const port = process.env.PORT || 3000
+
+app.listen(3000, () => console.log(`Example app listening on port ${port}`))
